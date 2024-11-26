686eb36db7d75dff369161af3108412c0ba55392
Akash Yadav <54095175+akash2056@users.noreply.github.com>
2024-11-26 01:17:49 -0500

    added login and signup api

---
+++ backend/database/userSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true },
+  phoneNumber: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, {
+  timestamps: true, // Automatically adds createdAt and updatedAt fields
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;

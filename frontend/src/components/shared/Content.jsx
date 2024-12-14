9732220629c5f1506df5539eba10533d9f3070be
Akash Yadav <54095175+akash2056@users.noreply.github.com>
2024-12-14 18:32:08 -0500

    Update Content.jsx

--- frontend/src/components/shared/Content.jsx
+++ frontend/src/components/shared/Content.jsx
@@ -1,3 +1,4 @@
+// Gurpreet and Prarthana
 import PropTypes from "prop-types";
 import Board from "../sudoku/Board";
 import { useState } from "react";
@@ -36,27 +37,6 @@ function Content({ boardDimension, difficulty, currentGameId, setCurrentGameId, 
           setBoardDimension={setBoardDimension}
         />
       </div>
-      
-      {/* Restart Game button styled similarly to Start Timer */}
-      {/* <button
-        onClick={restartGame}
-        style={{
-          border: "2px solid #558B71",
-          backgroundColor: "#7fcfa8",
-          color: "white",
-          padding: "10px 15px",
-          cursor: "pointer",
-          fontSize: "1rem",
-          fontWeight: "bold",
-          borderRadius: "8px",
-          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.15)",
-          fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
-          transition: "all 0.3s ease",
-        }}
-        className="mt-4"
-      >
-        Restart Game
-      </button> */}
 
       {showDifficultyDialog && difficultyText}
     </div>

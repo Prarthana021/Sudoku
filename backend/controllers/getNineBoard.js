c200475a913ce16c1b1109b1d233abd4a1f85e5f
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2024-12-14 18:26:05 -0500

    Update getNineBoard.js

--- backend/controllers/getNineBoard.js
+++ backend/controllers/getNineBoard.js
@@ -1,23 +1,18 @@
+// Akash & Gurpreet
+
 import { generateBoard } from "../helpers/generateBoard.js";
 import createGame from "../helpers/createGame.js";
 
 export const getNineBoard = async (req, res, next) => {
   try {
     const id = parseInt(req.params.id);
-    // Check if id is not equal to 9
     if (id !== 9) {
-      // Assuming you want to skip the rest of the middleware stack
       return {
         message: "Bad request",
       };
-      // If you want to send a response, use res.status().json() here
     }
     if (id === 9) {
-      // Generate a 9x9 Sudoku board and its solution
       const dic = generateBoard(9, 9);
-      // console.log(dic);
-      // Create and save games
-
       const board = dic["cell"];
       const solution = dic["solution"];
       const game = await createGame(board, solution, 9, []);

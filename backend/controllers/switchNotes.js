85e22fa908f0ec278010233875ac326f422db29b
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2024-12-14 18:30:30 -0500

    Update switchNotes.js

--- backend/controllers/switchNotes.js
+++ backend/controllers/switchNotes.js
@@ -1,14 +1,12 @@
+// Aakash
+
 import Game from "../database/gameSchema.js";
 import { ObjectId } from "mongodb";
 import updateGame from "../helpers/updateGame.js";
 
-// @params{
-// id on the url
-//  }
 const switchNote = async (req, res) => {
   try {
-    // note mode on
-    //coordinate of the board
+
     const gameId = new ObjectId(req.params.id);
     const game = await Game.findOne({ _id: gameId });
     const problemBoard = game["problemBoard"];

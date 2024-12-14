7c207e3c62f673093e1447174f3c5b0a872f0b37
Prarthana Shiwakoti <prarthanaaa.2012@gmail.com>
2024-12-14 18:06:11 -0500

    correct so far

--- backend/controllers/correctSoFar.js
+++ backend/controllers/correctSoFar.js
@@ -1,27 +1,4 @@
-// import checkIfValid from "../helpers/checkIfValid.js";
-// import Game from "../database/gameSchema.js";
-// import { ObjectId } from "mongodb";
-// //json {valid:true/false}
-// export const correctSoFar = async (req, res) => {
-//   try {
-//     const gameId = new ObjectId(req.params.id);
-//     //const board = JSON.parse(req.body.board.matrix);
-//     let board = await Game.findOne({ _id: gameId });
-//     board = board["problemBoard"];
-
-//     if (!Array.isArray(board) || board.length === 0 || !board.every((row) => Array.isArray(row))) {
-//       // Invalid board structure
-//       return res.status(400).json({ error: "Invalid board structure" });
-//     }
-
-//     const isValid = checkIfValid(board);
-
-//     return res.json({ valid: isValid });
-//   } catch (error) {
-//     console.error("Error checking if the board is correct so far:", error);
-//     return res.status(500).json({ message: "Internal Server Error", error });
-//   }
-// };
+//Prarthana
 
 import checkIfValid from "../helpers/checkIfValid.js";
 import Game from "../database/gameSchema.js";
@@ -46,7 +23,6 @@ export const correctSoFar = async (req, res) => {
     const incorrectCells = [];
 
     if (!isValid) {
-      // Logic to find incorrect cells (this could vary based on your validation logic)
       for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[row].length; col++) {
           if (board[row][col].value !== board[row][col].correctValue) {  // Assuming you have the correct value stored
